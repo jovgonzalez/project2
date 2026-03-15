c41d201b890108e501e00bf1217d60bd1f8e7ce3
jovgonzalez <jovgonzalez@gmail.com>
2026-03-14 21:56:08 -0500

    fixed java script in eclipse

--- FirstProject/src/test/java/AppTest.java
+++ FirstProject/src/test/java/AppTest.java
@@ -1,4 +1,11 @@
+package com.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest {
-
+    @Test
+    public void testBasic() {
+        assertTrue(1 + 1 == 2);
+    }
 }
